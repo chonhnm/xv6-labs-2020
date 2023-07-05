05934577cc662f46c628ae1b3e9f8097d1ceafef
chon <chonhnm@gmail.com>
2023-07-05 23:21:14 +0800

    util lab: sleep

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int n;
+
+    if (argc != 2)
+    {
+        fprintf(2, "usage: sleep N\n");
+        exit(1);
+    }
+
+    n = atoi(argv[1]);
+    sleep(n);
+    exit(0);
+}
