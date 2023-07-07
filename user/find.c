a039559b17e91a6330dd57a1b662394e43a7b1d3
chon <chonhnm@gmail.com>
2023-07-08 00:23:37 +0800

    find: fix bug

--- user/find.c
+++ user/find.c
@@ -28,86 +28,94 @@ void find(char *path, char *pattern)
         return;
     }
 
-    if (fstat(fd, &st) < 0)
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
     {
-        fprintf(2, "find: cannot stat %s\n", path);
+        printf("find: path too long %s\n", path);
         close(fd);
         return;
     }
-
-    switch (st.type)
+    strcpy(buf, path);
+    p = buf + strlen(buf);
+    *p++ = '/';
+    while (read(fd, &de, sizeof(de)) == sizeof(de))
     {
-    case T_DEVICE:
-    case T_FILE:
-        if (strcmp(fmtname(path), pattern) == 0)
+        if (de.inum == 0)
         {
-            printf("%s\n", path);
+            continue;
         }
-        break;
-
-    case T_DIR:
-        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if (stat(buf, &st) < 0)
         {
-            printf("find: path too long\n");
+            printf("find: cannot stat %s\n", buf);
+            continue;
+        }
+
+        switch (st.type)
+        {
+        case T_DEVICE:
+        case T_FILE:
+            if (strcmp(fmtname(de.name), pattern) == 0)
+            {
+                printf("%s\n", buf);
+            }
+            break;
+
+        case T_DIR:
+            if (strcmp(".", de.name) == 0 ||
+                strcmp("..", de.name) == 0)
+            {
+                continue;
+            }
+            find(buf, pattern);
             break;
         }
-        strcpy(buf, path);
-        p = buf + strlen(buf);
-        *p++ = '/';
-        while (read(fd, &de, sizeof(de)) == sizeof(de))
-        {
-            if (de.inum == 0)
-            {
-                continue;
-            }
-            memmove(p, de.name, DIRSIZ);
-            p[DIRSIZ] = 0;
-            if (stat(buf, &st) < 0)
-            {
-                printf("find: cannot stat %s\n", buf);
-                continue;
-            }
-
-            switch (st.type)
-            {
-            case T_DEVICE:
-            case T_FILE:
-                if (strcmp(fmtname(de.name), pattern) == 0)
-                {
-                    printf("%s\n", buf);
-                }
-                break;
-
-            case T_DIR:
-                if (strcmp(".", de.name) == 0 ||
-                    strcmp("..", de.name) == 0)
-                {
-                    continue;
-                }
-                find(buf, pattern);
-                break;
-            }
-        }
     }
+    close(fd);
 }
 
 int main(int argc, char *argv[])
 {
     int i;
+    char *path;
+    int fd;
+    struct stat st;
+
     if (argc < 2)
     {
-        fprintf(2, "Usage: find <DIR> <FILE_NAME>");
+        fprintf(2, "Usage: find [<DIR>] <FILE_NAME> ");
         exit(1);
     }
 
-    if (argc == 2)
+    path = argv[1];
+
+    if ((fd = open(path, 0)) < 0)
     {
-        find(".", argv[1]);
-        exit(0);
+        fprintf(2, "find: cannot open %s \n", path);
+        exit(1);
     }
-    for (i = 2; i < argc; i++)
+
+    if (fstat(fd, &st) < 0)
     {
-        find(argv[1], argv[i]);
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        exit(1);
     }
+
+    if (st.type == T_DIR)
+    {
+        for (i = 2; i < argc; i++)
+        {
+            find(path, argv[i]);
+        }
+    }
+    else
+    {
+        for (i = 1; i < argc; i++)
+        {
+            find(".", argv[i]);
+        }
+    }
+    close(fd);
     exit(0);
 }
