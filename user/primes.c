1ea20b9e606c8f911cc8749df09303da1fa137c2
chon <chonhnm@gmail.com>
2023-07-09 14:06:16 +0800

    xargs

--- user/primes.c
+++ user/primes.c
@@ -2,111 +2,91 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-__attribute__((noreturn)) void loopIt(int *pfd)
-{
-    int buf;
-    int base;
-    int needFolk = 0;
-    int cfd[2];
-    int cpid;
+__attribute__((noreturn)) void loopIt(int *pfd) {
+  int buf;
+  int base;
+  int needFolk = 0;
+  int cfd[2];
+  int cpid;
 
-    close(pfd[1]);
+  close(pfd[1]);
 
-    if (read(pfd[0], &buf, 1) == 0)
-    {
-        exit(0);
+  if (read(pfd[0], &buf, sizeof(buf)) == 0) {
+    exit(0);
+  }
+
+  // print prime
+  base = buf;
+  printf("prime %d\n", base);
+
+  while (read(pfd[0], &buf, sizeof(buf)) > 0) {
+    if (buf % base) {
+      needFolk = 1;
+      break;
     }
+  }
 
-    // print prime
-    base = buf;
-    printf("prime %d\n", base);
+  if (!needFolk) {
+    exit(0);
+  }
 
-    while (read(pfd[0], &buf, 1) > 0)
-    {
-        if (buf % base != 0)
-        {
-            needFolk = 1;
-            break;
-        }
-    }
+  // pipe
+  if (pipe(cfd) == -1) {
+    fprintf(2, "pipe error\n");
+    exit(1);
+  }
 
-    if (!needFolk)
-    {
-        exit(0);
-    }
+  cpid = fork();
+  if (cpid == -1) {
+    fprintf(2, "fork error\n");
+    exit(1);
+  }
 
-    // pipe
-    if (pipe(cfd) == -1)
-    {
-        fprintf(2, "pipe error\n");
-        exit(1);
+  if (cpid == 0) {
+    loopIt(cfd);
+  } else {
+    close(cfd[0]);
+    write(cfd[1], &buf, sizeof(buf));
+    while (read(pfd[0], &buf, sizeof(buf)) > 0) {
+      if (buf % base) {
+        write(cfd[1], &buf, sizeof(buf));
+      }
     }
-
-    cpid = fork();
-    if (cpid == -1)
-    {
-        fprintf(2, "fork error\n");
-        exit(1);
-    }
-
-    if (cpid == 0)
-    {
-        loopIt(cfd);
-    }
-    else
-    {
-        close(cfd[0]);
-        write(cfd[1], &buf, 1);
-        while (read(pfd[0], &buf, 1) > 0)
-        {
-            if (buf % base != 0)
-            {
-                write(cfd[1], &buf, 1);
-            }
-        }
-        close(pfd[0]);
-        close(cfd[1]);
-        wait(0);
-        exit(0);
-    }
+    close(pfd[0]);
+    close(cfd[1]);
+    wait(0);
+    exit(0);
+  }
 }
 
-int main(int argc, char *argv[])
-{
-    int fd[2];
-    int cpid;
+int main(int argc, char *argv[]) {
+  int fd[2];
+  int cpid;
 
-    printf("prime %d\n", 2);
+  printf("prime %d\n", 2);
 
-    if (pipe(fd) == -1)
-    {
-        fprintf(2, "pipe error\n");
-        exit(1);
-    }
+  if (pipe(fd) == -1) {
+    fprintf(2, "pipe error\n");
+    exit(1);
+  }
 
-    cpid = fork();
-    if (cpid == -1)
-    {
-        fprintf(2, "fork error\n");
-        exit(1);
-    }
+  cpid = fork();
+  if (cpid == -1) {
+    fprintf(2, "fork error\n");
+    exit(1);
+  }
 
-    if (cpid == 0)
-    {
-        loopIt(fd);
+  if (cpid == 0) {
+    loopIt(fd);
+  } else {
+    close(fd[0]);
+    for (int i = 3; i <= 35; i++) {
+      if (i % 2) {
+        write(fd[1], &i, sizeof(i));
+      }
     }
-    else
-    {
-        close(fd[0]);
-        for (int i = 3; i <= 35; i++)
-        {
-            if (i % 2 != 0)
-            {
-                write(fd[1], &i, 1);
-            }
-        }
-        close(fd[1]);
-        wait(0);
-        exit(0);
-    }
+    close(fd[1]);
+    wait(0);
+    exit(0);
+  }
 }
--- user/xargs.c
+++ user/xargs.c
@@ -1,4 +1,119 @@
+#include "kernel/param.h"
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "kernel/param.h"
 #include "user/user.h"
+
+struct cmdarg {
+  char *argv[MAXARG];
+  char *eargv[MAXARG];
+};
+
+__attribute__((noreturn)) void runcmd(struct cmdarg *arg) {
+  if (arg->argv[0] == 0) {
+    fprintf(2, "no exec file\n");
+    exit(1);
+  }
+  exec(arg->argv[0], arg->argv);
+  fprintf(2, "exec %s failed\n", arg->argv[0]);
+  exit(1);
+}
+
+struct cmdarg *cmdarg(void) {
+  struct cmdarg *ret;
+  ret = malloc(sizeof(*ret));
+  memset(ret, 0, sizeof(*ret));
+  return ret;
+}
+
+int readline(char *buf, int nbuf) {
+  memset(buf, 0, nbuf);
+  gets(buf, nbuf);
+  if (buf[0] == 0) { // EOF
+    return -1;
+  }
+  return 0;
+}
+
+char whitespace[] = " \t\r\n\v";
+
+int gettoken(char **ps, char *es, char **q, char **eq) {
+  char *s;
+  int ret;
+
+  s = *ps;
+  while (s < es && strchr(whitespace, *s)) {
+    s++;
+  }
+  if (q) {
+    *q = s;
+  }
+  ret = *s;
+  while (s < es && !strchr(whitespace, *s)) {
+    s++;
+  }
+  if (eq) {
+    *eq = s;
+  }
+  while (s < es && strchr(whitespace, *s)) {
+    s++;
+  }
+  *ps = s;
+  if (*q == *eq) {
+    return 0;
+  }
+  return ret;
+}
+
+void parsecmd(struct cmdarg *cmdarg, char *buf, int argc) {
+  char **ps, *es;
+  char *q, *eq;
+  int i;
+
+  ps = &buf;
+  es = buf + strlen(buf);
+  while (gettoken(ps, es, &q, &eq) > 0) {
+    cmdarg->argv[argc] = q;
+    cmdarg->eargv[argc] = eq;
+    argc++;
+    if (argc >= MAXARG) {
+      fprintf(2, "too many args");
+      exit(1);
+    }
+  }
+  cmdarg->argv[argc] = 0;
+  cmdarg->eargv[argc] = 0;
+
+  // null terminate
+  for (i = 0; i < argc; i++) {
+    if (cmdarg->argv[i] == 0) {
+      break;
+    }
+    if (cmdarg->eargv[i] != 0) {
+      *cmdarg->eargv[i] = 0;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  char buf[100];
+  struct cmdarg *ret;
+  int i;
+
+  if (argc < 2) {
+    fprintf(2, "Usage: xargs <CMD> [PARAMS]");
+    exit(1);
+  }
+
+  ret = cmdarg();
+  for (i = 0; i < argc - 1; i++) {
+    ret->argv[i] = argv[i + 1];
+  }
+  while (readline(buf, sizeof(buf)) >= 0) {
+    if (fork() == 0) {
+      parsecmd(ret, buf, i);
+      runcmd(ret);
+    }
+    wait(0);
+  }
+  exit(0);
+}
